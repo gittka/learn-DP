a4147dfed2c87ebfc46673b2c1e49dc282bb234d
@lex <harryspringts@gmail.com>
2023-03-08 03:15:17 +0100

    Find accounts with predicate

--- src/main/java/repository/AccountRepository.java
+++ src/main/java/repository/AccountRepository.java
@@ -4,10 +4,12 @@ import main.java.model.BankAccount;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 public interface AccountRepository {
     BankAccount save(BankAccount bankAccount);
     List<BankAccount> findAll();
     Optional<BankAccount> findById(Long id);
+    List<BankAccount> findAccounts(Predicate<BankAccount> predicate);
 
 }
