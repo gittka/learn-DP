dc957c3347c6be0c634155171d48c69148d482a3
@lex <harryspringts@gmail.com>
2023-03-14 21:15:00 +0100

    Resolving Conflict in MainNew.java

--- src/MainNew.java
+++ src/MainNew.java
@@ -6,12 +6,6 @@ import main.java.model.Director;
 
 public class MainNew {
     public static void main(String[] args) {
-        BankAccount bankAccount = Director.builder()
-                .accountId(2L)
-                .accountBalance(100.0)
-                .accountCurrency("USD")
-                .accountStatus(AccountStatus.CREATED)
-                .accountType(AccountType.CURRENT_ACCOUNT)
-                .build();
+
     }
 }
